8c6fef20c7115c004d2363bebbef59f38ab14942
Apel Mahmud <apelmahmud.dev@gmail.com>
2022-11-29 10:11:56 +0600

    package: 📦 readux

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,6 +15,16 @@ module.exports = {
 			secondary: "#FF9F43",
 			"primary-light": "#ffdf0f",
 		},
+		container: {
+			center: true,
+			padding: {
+				DEFAULT: "1rem",
+				sm: "2rem",
+				lg: "4rem",
+				xl: "5rem",
+				"2xl": "6rem",
+			},
+		},
 	},
 	plugins: [],
 };
